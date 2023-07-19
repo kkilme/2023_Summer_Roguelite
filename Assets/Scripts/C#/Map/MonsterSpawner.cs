56a123b6084cc538e346d640a3959d3148a0754f
Bazzi4263 <hkr0106567@gmail.com>
2023-07-20 00:27:42 +0900

    몬스터 스포너 방 안에 체크하는 함수 추가

--- Assets/Scripts/C#/Map/MonsterSpawner.cs
+++ Assets/Scripts/C#/Map/MonsterSpawner.cs
@@ -8,7 +8,7 @@ public class MonsterSpawner : NetworkBehaviour
     [SerializeField] private ROOMSIZE roomSize;
     [SerializeField] private Transform[] spawnerPoses;
 
-    // 룸안의 랜덤한 좌표를 리턴함
+    // 룸안의 랜덤한 좌표를 리턴하는 함수
     public Vector3 GetRandomRoomPos()
     {
         Vector3 pos = transform.position;
@@ -23,6 +23,24 @@ public class MonsterSpawner : NetworkBehaviour
                 return new Vector3(Random.Range(pos.x - 32, pos.x + 32), 1, Random.Range(pos.z - 19, pos.z + 19));
             default:
                 return Vector3.zero;
+        }
+    }
+
+    // 해당 좌표가 방안에 해당하는지 리턴하는 함수
+    public bool IsPosInRoom(Vector3 pos)
+    {
+        Vector3 roomPos = transform.position;
+
+        switch (roomSize)
+        {
+            case ROOMSIZE.SMALL:
+                return roomPos.x - 12 <= pos.x && pos.x <= roomPos.x + 12 && roomPos.z - 12 <= pos.z && pos.z <= roomPos.z + 12;
+            case ROOMSIZE.MEDIUM:
+                return roomPos.x - 19 <= pos.x && pos.x <= roomPos.x + 19 && roomPos.z - 19 <= pos.z && pos.z <= roomPos.z + 19;
+            case ROOMSIZE.LARGE:
+                return roomPos.x - 32 <= pos.x && pos.x <= roomPos.x + 32 && roomPos.z - 19 <= pos.z && pos.z <= roomPos.z + 19;
+            default:
+                return false;
         }
     }
 
